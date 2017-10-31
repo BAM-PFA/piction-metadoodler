c3827c239cf74a776951991a2f2707bd515bd2d7
mcampos-quinn <mcq@berkeley.edu>
2017-10-31 14:45:59 -0700

    some cleanup, started updating to bootstrap layout

--- ftp.php
+++ ftp.php
@@ -5,12 +5,9 @@ function ftpFile($source,$ftpDir,$basename){
 	$conn_id  = ftp_connect($pictionServer) or die("Could not connect to $pictionServer");
 	$login = ftp_login($conn_id , 'bampfa', 'QDp4Tp25');
 	
-	echo "$remotefilepath<br>";
-	echo "$source<br>";
-
 	if (ftp_put($conn_id, $remoteFile, $source, FTP_BINARY))
 	  {
-	  echo "Successfully uploaded $basename.";
+	  echo "Successfully uploaded $basename.<br>";
 	  }
 	else
 	  {
--- metadata.php
+++ metadata.php
@@ -11,6 +11,7 @@ $targetdir = '/Users/michael/Sites/pictionHelper/uploads/';
 // echo "/<pre>"; 
 
 $METADATA = "FILENAME,BAMPFA.TITLE,BAMPFA.ARTIST,BAMPFA.YEAR,EVENTS.DC-COVERAGE,EVENTS.DC-PUBLISHER,EVENTS.RELATED_EXHIBITIONS,EVENTS.EVENT_LOCATION,EVENTS.DC-DESCRIPTION,EVENTS.DC-TYPE,EVENTS.DC-SUBJECT,EVENTS.TOPICAL_SUBJECT,EVENTS.ORGANIZER,EVENTS.DC-CONTRIBUTOR,EVENTS.DC-RIGHTS,EVENTS.DC-AUTHOR,EVENTS.TAGS,EVENTS.BAM_PFA_CAPTION,EVENTS.RESTRICTIONS,EVENTS.DC-CREATOR,FILM.BAM_PFA_CAPTION,FILM.DC-CONTRIBUTOR,FILM.DC-CREATOR,FILM.DC-DESCRIPTION,FILM.DC-PUBLISHER,FILM.DC-RIGHTS,FILM.DC-SUBJECT,FILM.DC-TYPE,FILM.RESTRICTIONS,FILM.TAGS,GALLERY EXHIBITION.ARTWORK_CREDIT_LINE,GALLERY EXHIBITION.ARTWORK_MEDIUM,GALLERY EXHIBITION.BAM_PFA_CAPTION,GALLERY EXHIBITION.CURATOR,GALLERY EXHIBITION.DC-CONTRIBUTOR,GALLERY EXHIBITION.DC-COVERAGE,GALLERY EXHIBITION.DC-CREATOR,GALLERY EXHIBITION.DC-DESCRIPTION,GALLERY EXHIBITION.DC-PUBLISHER,GALLERY EXHIBITION.DC-RIGHTS,GALLERY EXHIBITION.DC-TITLE,GALLERY EXHIBITION.DC-TYPE,GALLERY EXHIBITION.EXHIBITION LOCATION,GALLERY EXHIBITION.FULL_EXHIBIT_DATE,GALLERY EXHIBITION.PHOTO_CREDIT,GALLERY EXHIBITION.RESTRICTIONS,GALLERY EXHIBITION.TAGS\n";
+$metadataFields = array("bampfatitle","bampfaartist","bampfayear","eventfulldate","eventseries","eventrelatedex","eventlocation","eventdescription","eventtype","eventpeople","eventsubject","eventorganizer","eventimagesource","eventimagecopyright","eventphotocredit","eventbampfacaption","eventrestrictions","eventtagarray","eventuploader","filmbampfacaption","filmcontrib","filmuploader","filmdescription","filmseries","filmrights","filmpeople","filmtype","filmtagarray","filmrestrictions","exhcreditline","exhmedium","exhcaption","exhcurator","exhsource","exhyearofexh","exhuploader","exhdescription","exhnameofexh","exhcopyright","exhartworktitle","exhimagetype","exhlocation","exhfulldates","exhphotocredit","exhtagarray","exhrestrictions");
 $category = $_POST['category'];
 $ftpDir = "\\zz_hothothotfolders\\" . $category . "\\";
 
@@ -20,75 +21,29 @@ foreach($_FILES['file']['tmp_name'] as $key => $tmp_name ){
     // $file_name = preg_replace('/\s+/', '_', $file_name);
     $file_tmp = $_FILES['file']['tmp_name'][$key];
     $basename = basename($file_name);
-    echo "$file_tmp\n\n";
-    echo "<div>" . $basename . "</div>";
-    $uploadedfile = $targetdir . basename($file_name);
-    if (move_uploaded_file($file_tmp, $uploadedfile)) {
-        // file uploaded succeeded
-        echo "success";
 
-        // ----- GENERATE CSV VALUES ----------
-        $filepath = "K:\\ftp_ucbcspace\BAMPFA\\" . $ftpDir . $basename;
-        $bampfatitle = $_POST['bampfatitle'];
-        $bampfaartist = $_POST['bampfaartist'];
-        $bampfayear = $_POST['bampfayear'];
-        $eventfulldate = $_POST['eventfulldate'];
-        $eventseries = $_POST['eventseries'];
-        $eventrelatedex = $_POST['eventrelatedex'];
-        $eventlocation = $_POST['eventlocation'];
-        $eventdescription = $_POST['eventdescription'];
-        $eventtype = $_POST['eventtype'];
-        $eventpeople = $_POST['eventpeople'];
-        $eventsubject = $_POST['eventsubject'];
-        $eventorganizer = $_POST['eventorganizer'];
-        $eventimagesource = $_POST['eventimagesource'];
-        $eventimagecopyright = $_POST['eventimagecopyright'];
-        $eventphotocredit = $_POST['eventphotocredit'];
-        $eventtagarray = $_POST['eventtagarray'];
-        $eventtags = implode(";", $eventtagarray);
-        $eventbampfacaption = $_POST['eventbampfacaption'];
-        $eventrestrictions = $_POST['eventrestrictions'];
-        $eventuploader = $_POST['eventuploader'];
-        $filmbampfacaption = $_POST['filmbampfacaption'];
-        $filmcontrib = $_POST['filmcontrib'];
-        $filmuploader = $_POST['filmuploader'];
-        $filmdescription = $_POST['filmdescription'];
-        $filmseries = $_POST['filmseries'];
-        $filmrights = $_POST['filmrights'];
-        $filmpeople = $_POST['filmpeople'];
-        $filmtype = $_POST['filmtype'];
-        $filmrestrictions = $_POST['filmrestrictions'];
-        $filmtagarray = $_POST['filmtagarray'];
-        $filmtags = implode(";", $filmtagarray);
-        $filmtags = $_POST['filmtags'];
-        $exhcreditline = $_POST['exhcreditline'];
-        $exhmedium = $_POST['exhmedium'];
-        $exhcaption = $_POST['exhcaption'];
-        $exhcurator = $_POST['exhcurator'];
-        $exhsource = $_POST['exhsource'];
-        $exhyearofexh = $_POST['exhyearofexh'];
-        $exhuploader = $_POST['exhuploader'];
-        $exhdescription = $_POST['exhdescription'];
-        $exhnameofexh = $_POST['exhnameofexh'];
-        $exhcopyright = $_POST['exhcopyright'];
-        $exhartworktitle = $_POST['exhartworktitle'];
-        $exhimagetype = $_POST['exhimagetype'];
-        $exhlocation = $_POST['exhlocation'];
-        $exhfulldates = $_POST['exhfulldates'];
-        $exhphotocredit = $_POST['exhphotocredit'];
-        $exhrestrictions = $_POST['exhrestrictions'];
-        $exhtagarray = $_POST['exhtagarray'];
-        $exhtags = implode(";", $exhtagarray);
-
-        $METADATA .= "$filepath,$bampfatitle,$bampfaartist,$bampfayear,$eventfulldate,$eventseries,$eventrelatedex,$eventlocation,$eventdescription,$eventtype,$eventpeople,$eventsubject,$eventorganizer,$eventimagesource,$eventimagecopyright,$eventphotocredit,$eventtags,$eventbampfacaption,$eventrestrictions,$eventuploader,$filmbampfacaption,$filmcontrib,$filmuploader,$filmdescription,$filmseries,$filmrights,$filmpeople,$filmtype,$filmrestrictions,$filmtags,$exhcreditline,$exhmedium,$exhcaption,$exhcurator,$exhsource,$exhyearofexh,$exhuploader,$exhdescription,$exhnameofexh,$exhcopyright,$exhartworktitle,$exhimagetype,$exhlocation,$exhfulldates,$exhphotocredit,$exhrestrictions,$exhtags\n";
-
-        ftpFile($uploadedfile,$ftpDir,$basename);
-
-    } else { 
-        // file upload failed
-        echo "FILE UPLOAD FOR ".$basename." FAILED";
+    // ----- GENERATE CSV VALUES ----------
+    $filepath = "K:\\ftp_ucbcspace\BAMPFA\\" . $ftpDir . $basename;
+    
+    foreach($metadataFields as $field){
+        if (!isset($_POST[$field])){
+            $$field = "";
+            if (strpos($$field, 'array')){
+                $$field = array();
+            }
+        }
+        else $$field = $_POST[$field];
 
     }
+    
+    $eventtags = implode(";", $eventtagarray);    
+    $filmtags = implode(";", $filmtagarray);
+    $exhtags = implode(";", $exhtagarray);
+
+    $METADATA .= "$filepath,$bampfatitle,$bampfaartist,$bampfayear,$eventfulldate,$eventseries,$eventrelatedex,$eventlocation,$eventdescription,$eventtype,$eventpeople,$eventsubject,$eventorganizer,$eventimagesource,$eventimagecopyright,$eventphotocredit,$eventtags,$eventbampfacaption,$eventrestrictions,$eventuploader,$filmbampfacaption,$filmcontrib,$filmuploader,$filmdescription,$filmseries,$filmrights,$filmpeople,$filmtype,$filmrestrictions,$filmtags,$exhcreditline,$exhmedium,$exhcaption,$exhcurator,$exhsource,$exhyearofexh,$exhuploader,$exhdescription,$exhnameofexh,$exhcopyright,$exhartworktitle,$exhimagetype,$exhlocation,$exhfulldates,$exhphotocredit,$exhrestrictions,$exhtags\n";
+
+    ftpFile($file_tmp,$ftpDir,$basename);
+
 }
 
 $metaCSVname = "/Users/michael/Sites/pictionHelper/uploads/metadata.csv";
