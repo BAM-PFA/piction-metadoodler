9fb72dce43e610c40fe2b76896fe8be1b53573f1
mcampos-quinn <mcq@berkeley.edu>
2017-11-03 17:09:03 -0700

    add image previews; killed POST function, oops

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
-function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+// function openCity(evt, cityName) {
+//     // Declare all variables
+//     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+//     // Get all elements with class="tabcontent" and hide them
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//         tabcontent[i].style.display = "none";
+//     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+//     // Get all elements with class="tablinks" and remove the class "active"
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//         tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+//     // Show the current tab, and add an "active" class to the button that opened the tab
+//     document.getElementById(cityName).style.display = "block";
+//     evt.currentTarget.className += " active";
+// }
 
-function adjust_textarea(h) {
-    h.style.height = "20px";
-    h.style.height = (h.scrollHeight)+"px";
-}
+// function adjust_textarea(h) {
+//     h.style.height = "20px";
+//     h.style.height = (h.scrollHeight)+"px";
+// }
 
 function addTag(divName,cat){
           var newdiv = document.createElement('div');
@@ -30,15 +30,17 @@ function addTag(divName,cat){
           document.getElementById(divName).appendChild(newdiv);
 }
 
-function hoverdiv(e,divid){ 
-    if ($('#doShowHints').is(':checked')){
-        var left = e.clientX + "px"; 
-        var top = e.clientY + "px"; 
+// function hoverdiv(e,divid){ 
+//     if ($('#doShowHints').is(':checked')){
+//         var left = e.clientX + "px"; 
+//         var top = e.clientY + "px"; 
 
-        $("#"+divid).css('left',left); 
-        $("#"+divid).css('top',top); 
-        $("#"+divid).css('position','fixed'); 
-        $("#"+divid).toggle(); 
-        return false; 
-        }
-}
+//         $("#"+divid).css('left',left); 
+//         $("#"+divid).css('top',top); 
+//         $("#"+divid).css('position','fixed'); 
+//         $("#"+divid).toggle(); 
+//         return false; 
+//         }
+// }
+
+
--- metadata.php
+++ metadata.php
@@ -1,5 +1,5 @@
 <?php
-echo "<html><body>";
+echo "<html><body>STARTING";
 
 include 'ftp.php';
 
@@ -7,13 +7,14 @@ $targetdir = '/Users/michael/Sites/pictionHelper/uploads/';
 // echo $_POST['tagarray'];
 // echo $_FILES;
 // echo "<pre>"; 
- // print_r($_FILES); 
+//  print_r($_FILES); 
 // echo "/<pre>"; 
 
 $METADATA = "FILENAME,BAMPFA.TITLE,BAMPFA.ARTIST,BAMPFA.YEAR,EVENTS.DC-COVERAGE,EVENTS.DC-PUBLISHER,EVENTS.RELATED_EXHIBITIONS,EVENTS.EVENT_LOCATION,EVENTS.DC-DESCRIPTION,EVENTS.DC-TYPE,EVENTS.DC-SUBJECT,EVENTS.TOPICAL_SUBJECT,EVENTS.ORGANIZER,EVENTS.DC-CONTRIBUTOR,EVENTS.DC-RIGHTS,EVENTS.DC-AUTHOR,EVENTS.TAGS,EVENTS.BAM_PFA_CAPTION,EVENTS.RESTRICTIONS,EVENTS.DC-CREATOR,FILM.BAM_PFA_CAPTION,FILM.DC-CONTRIBUTOR,FILM.DC-CREATOR,FILM.DC-DESCRIPTION,FILM.DC-PUBLISHER,FILM.DC-RIGHTS,FILM.DC-SUBJECT,FILM.DC-TYPE,FILM.RESTRICTIONS,FILM.TAGS,GALLERY EXHIBITION.ARTWORK_CREDIT_LINE,GALLERY EXHIBITION.ARTWORK_MEDIUM,GALLERY EXHIBITION.BAM_PFA_CAPTION,GALLERY EXHIBITION.CURATOR,GALLERY EXHIBITION.DC-CONTRIBUTOR,GALLERY EXHIBITION.DC-COVERAGE,GALLERY EXHIBITION.DC-CREATOR,GALLERY EXHIBITION.DC-DESCRIPTION,GALLERY EXHIBITION.DC-PUBLISHER,GALLERY EXHIBITION.DC-RIGHTS,GALLERY EXHIBITION.DC-TITLE,GALLERY EXHIBITION.DC-TYPE,GALLERY EXHIBITION.EXHIBITION LOCATION,GALLERY EXHIBITION.FULL_EXHIBIT_DATE,GALLERY EXHIBITION.PHOTO_CREDIT,GALLERY EXHIBITION.RESTRICTIONS,GALLERY EXHIBITION.TAGS\n";
 $metadataFields = array("bampfatitle","bampfaartist","bampfayear","eventfulldate","eventseries","eventrelatedex","eventlocation","eventdescription","eventtype","eventpeople","eventsubject","eventorganizer","eventimagesource","eventimagecopyright","eventphotocredit","eventbampfacaption","eventrestrictions","eventtagarray","eventuploader","filmbampfacaption","filmcontrib","filmuploader","filmdescription","filmseries","filmrights","filmpeople","filmtype","filmtagarray","filmrestrictions","exhcreditline","exhmedium","exhcaption","exhcurator","exhsource","exhyearofexh","exhuploader","exhdescription","exhnameofexh","exhcopyright","exhartworktitle","exhimagetype","exhlocation","exhfulldates","exhphotocredit","exhtagarray","exhrestrictions");
 $category = $_POST['category'];
 $ftpDir = "\\zz_hothothotfolders\\" . $category . "\\";
+echo "<div> THIS IS POST: ".getcwd()."</div>";
 
 //  ------  MOVE FILES -------------
 foreach($_FILES['file']['tmp_name'] as $key => $tmp_name ){
@@ -21,10 +22,10 @@ foreach($_FILES['file']['tmp_name'] as $key => $tmp_name ){
     // $file_name = preg_replace('/\s+/', '_', $file_name);
     $file_tmp = $_FILES['file']['tmp_name'][$key];
     $basename = basename($file_name);
-
+    echo $file_name;
     // ----- GENERATE CSV VALUES ----------
     $filepath = "K:\\ftp_ucbcspace\BAMPFA\\" . $ftpDir . $basename;
-    
+    echo $filepath;
     foreach($metadataFields as $field){
         if (!isset($_POST[$field])){
             $$field = "";
