ff5daceca1af6e66cfb46f59152ae64851a0a531
mcampos-quinn <mcq@berkeley.edu>
2017-11-15 14:06:54 -0800

    all basic functions working; started working on tooltips for metadata entry

--- ftp.php
+++ ftp.php
@@ -1,11 +1,14 @@
 <?php
 function ftpFile($source,$ftpDir,$basename){
 	$remoteFile = "$ftpDir$basename";
+	echo $source . "....". $ftpDir . "....". $basename . "....". $remoteFile;
 	$pictionServer = 'ucb1.piction.com';
 	$conn_id  = ftp_connect($pictionServer) or die("Could not connect to $pictionServer");
 	$login = ftp_login($conn_id , 'bampfa', 'QDp4Tp25');
-	
-	if (ftp_put($conn_id, $remoteFile, $source, FTP_BINARY))
+
+	ftp_chdir($conn_id, $ftpDir);
+
+	if (ftp_put($conn_id, $basename, $source, FTP_BINARY))
 	  {
 	  echo "Successfully uploaded $basename.<br>";
 	  }
--- metadata.php
+++ metadata.php
@@ -3,15 +3,18 @@ echo "<html><body>";
 
 include 'ftp.php';
 
-// echo "<pre>"; 
-// print_r($_POST); 
-// echo "/<pre>"; 
+echo "<pre>"; 
+print_r($_FILES); 
+echo "/<pre>"; 
 
 $METADATA = "FILENAME,BAMPFA.TITLE,BAMPFA.ARTIST,BAMPFA.YEAR,EVENTS.DC-COVERAGE,EVENTS.DC-PUBLISHER,EVENTS.RELATED_EXHIBITIONS,EVENTS.EVENT_LOCATION,EVENTS.DC-DESCRIPTION,EVENTS.DC-TYPE,EVENTS.DC-SUBJECT,EVENTS.TOPICAL_SUBJECT,EVENTS.ORGANIZER,EVENTS.DC-CONTRIBUTOR,EVENTS.DC-RIGHTS,EVENTS.DC-AUTHOR,EVENTS.TAGS,EVENTS.BAM_PFA_CAPTION,EVENTS.RESTRICTIONS,EVENTS.DC-CREATOR,FILM.BAM_PFA_CAPTION,FILM.DC-CONTRIBUTOR,FILM.DC-CREATOR,FILM.DC-DESCRIPTION,FILM.DC-PUBLISHER,FILM.DC-RIGHTS,FILM.DC-SUBJECT,FILM.DC-TYPE,FILM.RESTRICTIONS,FILM.TAGS,GALLERY EXHIBITION.ARTWORK_CREDIT_LINE,GALLERY EXHIBITION.ARTWORK_MEDIUM,GALLERY EXHIBITION.BAM_PFA_CAPTION,GALLERY EXHIBITION.CURATOR,GALLERY EXHIBITION.DC-CONTRIBUTOR,GALLERY EXHIBITION.DC-COVERAGE,GALLERY EXHIBITION.DC-CREATOR,GALLERY EXHIBITION.DC-DESCRIPTION,GALLERY EXHIBITION.DC-PUBLISHER,GALLERY EXHIBITION.DC-RIGHTS,GALLERY EXHIBITION.DC-TITLE,GALLERY EXHIBITION.DC-TYPE,GALLERY EXHIBITION.EXHIBITION LOCATION,GALLERY EXHIBITION.FULL_EXHIBIT_DATE,GALLERY EXHIBITION.PHOTO_CREDIT,GALLERY EXHIBITION.RESTRICTIONS,GALLERY EXHIBITION.TAGS\n";
 $metadataFields = array("bampfatitle","bampfaartist","bampfayear","eventfulldate","eventseries","eventrelatedex","eventlocation","eventdescription","eventtype","eventpeople","eventsubject","eventorganizer","eventimagesource","eventimagecopyright","eventphotocredit","eventbampfacaption","eventrestrictions","eventtagarray","eventuploader","filmbampfacaption","filmcontrib","filmuploader","filmdescription","filmseries","filmrights","filmpeople","filmtype","filmtagarray","filmrestrictions","exhcreditline","exhmedium","exhcaption","exhcurator","exhsource","exhyearofexh","exhuploader","exhdescription","exhnameofexh","exhcopyright","exhartworktitle","exhimagetype","exhlocation","exhfulldates","exhphotocredit","exhtagarray","exhrestrictions");
 $METADATA_VALUES = "";
 $category = $_POST['category'];
-$ftpDir = "K:\\ftp_ucbcspace\BAMPFA\\zz_hothothotfolders\\" . $category . "\\";
+echo "<div><b>".$category."</b></div>";
+// $ftpDir = "K:\\ftp_ucbcspace\\BAMPFA\\zz_hothothotfolders\\" . $category . "\\";
+$ftpDir = "zz_hothothotfolders\\" . $category;
+echo $ftpDir;
 
 //  ------  GET TEMP FILES -------------
 foreach($_FILES['file']['tmp_name'] as $key => $tmp_name ){
@@ -46,7 +49,7 @@ foreach($_FILES['file']['tmp_name'] as $key => $tmp_name ){
 
 date_default_timezone_set('America/Los_Angeles');
 $today = date('Y-m-d');
-$metaCSVname = getcwd() . "/uploads/" . $today . $category . "metadata.csv";
+$metaCSVname = getcwd() . "/uploads/" . $today . "_" . $category . "_metadata.csv";
 // use this to get the date in the metadata file --> substr($metaCSVname,0,10)
 
 // -------- IF METADATA CSV ALREADY EXISTS, APPEND TO IT, OTHERWISE CREATE IT 
