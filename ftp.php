2c8ace56bcbb17979fe09e2c52df7e38dc338c8b
mcampos-quinn <mcq@berkeley.edu>
2018-01-18 15:29:40 -0800

    add exhrights to output csv (maps to xmp.rights)

--- ftp.php
+++ ftp.php
@@ -11,7 +11,7 @@ function ftpFile($source,$ftpDir,$basename){
 
 	$conn_id = ftp_connect($pictionServer);
 	// read credentials from a file outside the documentroot
-	$ftp_credential_array = file('../../ftp/ftp.txt',FILE_IGNORE_NEW_LINES);
+	$ftp_credential_array = file('/var/www/ftp/ftp.txt',FILE_IGNORE_NEW_LINES);
 	
 	$username = $ftp_credential_array[0];
 	$password = $ftp_credential_array[1];
